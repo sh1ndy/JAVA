9000526df3bb9d537d669ffb85a4d5f4319d52b1
yeol2 <43854344+sh1ndy@users.noreply.github.com>
2022-02-08 18:50:39 +0900

    Create Main.java

---
+++ Swing/GUISwing/ch04_buttons/Main.java
@@ -0,0 +1,13 @@
+package ch04_buttons;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// JButton = a button that performs an action when clicked on
+		
+		MyFrame frame = new MyFrame();
+
+	}
+
+}

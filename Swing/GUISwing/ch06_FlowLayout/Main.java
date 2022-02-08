b6720c24ac2d8d2fe2fd1607ef9d43ca47e21b04
yeol2 <43854344+sh1ndy@users.noreply.github.com>
2022-02-08 23:26:55 +0900

    Create Main.java

---
+++ Swing/GUISwing/ch06_FlowLayout/Main.java
@@ -0,0 +1,50 @@
+package ch06_FlowLayout;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Layout Manager = Defines the natural layout for components within a container
+		
+		// FlowLayout = places components in a row, sized at their preferred size.
+		//				If the horizontal space in the container is too small,
+		//				the FlowLayout class uses the next available row.
+		
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 500);
+		//frame.setLayout(new FlowLayout(FlowLayout.LEADING));	// 왼쪽 기준으로 반응형
+		//frame.setLayout(new FlowLayout(FlowLayout.CENTER));	// 중앙 기준으로 반응형
+		//frame.setLayout(new FlowLayout(FlowLayout.TRAILING));	// 오른쪽 기준으로 반응형
+		frame.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));	// 매개변수(FloayLayout.중앙 기준, 좌우 간격, 높이 간격)
+		
+		JPanel panel = new JPanel();
+		panel.setPreferredSize(new Dimension(100, 250));
+		panel.setBackground(Color.lightGray);
+		panel.setLayout(new FlowLayout());
+		
+		panel.add(new JButton("1"));
+		panel.add(new JButton("2"));
+		panel.add(new JButton("3"));
+		panel.add(new JButton("4"));
+		panel.add(new JButton("5"));
+		panel.add(new JButton("6"));
+		panel.add(new JButton("7"));
+		panel.add(new JButton("8"));
+		panel.add(new JButton("0"));
+		
+		frame.add(panel);
+		frame.setVisible(true);
+		
+		
+	}
+
+}

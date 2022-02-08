cda4852d3e2df301d406ca71f0c0b9b0aac261dd
yeol2 <43854344+sh1ndy@users.noreply.github.com>
2022-02-08 18:48:44 +0900

    Create Main.java

---
+++ Swing/GUISwing/ch01_Frame/Main.java
@@ -0,0 +1,32 @@
+package ch01_Frame;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		/*
+		// JFrame = a GUI window to add components to
+		// JFrame = 구성 요소를 추가하는 GUI 창
+
+		JFrame frame = new JFrame(); // creates a frame
+		frame.setTitle("JFrame title goise here"); // sets title of frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
+		frame.setResizable(false); // prevent frame from being resized
+		frame.setSize(420, 420); // set the x-dimension, and y-dimension of frame
+		frame.setVisible(true); // make frame visible
+
+		ImageIcon image = new ImageIcon("logo.png"); // create an ImageIcon
+		frame.setIconImage(image.getImage()); // change icon of frame;
+		frame.getContentPane().setBackground(new Color(123, 50, 250)); // change color of background
+		*/
+		
+		MyFrame myFrame = new MyFrame();
+
+	}
+
+}

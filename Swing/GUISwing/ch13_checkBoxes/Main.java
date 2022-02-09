9d94a7334df47aed20af26054fb26908229762f8
yeol2 <43854344+sh1ndy@users.noreply.github.com>
2022-02-09 20:52:49 +0900

    Create Main.java

---
+++ Swing/GUISwing/ch13_checkBoxes/Main.java
@@ -0,0 +1,13 @@
+package ch13_checkboxes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// JCheckBox = A GUI component that can be selected or deselected
+		
+		new MyFrame();
+
+	}
+
+}

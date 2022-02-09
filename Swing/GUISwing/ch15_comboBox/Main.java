0bc0ff116812abc163a9255d30f234c8907de83a
yeol2 <43854344+sh1ndy@users.noreply.github.com>
2022-02-09 20:54:14 +0900

    Create Main.java

---
+++ Swing/GUISwing/ch15_comboBox/Main.java
@@ -0,0 +1,13 @@
+package ch15_comboBoxes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// JComboBox = A component that combines a button or editable field and a drop-down list
+		
+		new MyFrame();
+
+	}
+
+}

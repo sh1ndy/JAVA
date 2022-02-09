85f0a0060cf2f0528c727fabb19b1d5afcbb609f
yeol2 <43854344+sh1ndy@users.noreply.github.com>
2022-02-09 20:53:28 +0900

    Create Main.java

---
+++ Swing/GUISwing/ch14_radioButtons/Main.java
@@ -0,0 +1,13 @@
+package ch14_radioButtons;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// JRadioButton = One or more buttons in a grouping in which only 1 may be selected per group
+		
+		new MyFrame();
+
+	}
+
+}

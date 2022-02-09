18dbdcd5c16a85ad9897b4b37acfeaac305ec41f
yeol2 <43854344+sh1ndy@users.noreply.github.com>
2022-02-09 20:51:42 +0900

    Create Main.java

---
+++ Swing/GUISwing/ch12_textfields/Main.java
@@ -0,0 +1,13 @@
+package ch12_textfields;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// JTextField = A GUI textbox component that can be used to add, set, or get text
+		
+		new MyFrame();
+		
+	}
+
+}

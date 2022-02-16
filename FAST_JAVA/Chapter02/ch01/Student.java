2f7b1a79a7ac7faa25c667ca2493db65989d9aec
yeol2 <43854344+sh1ndy@users.noreply.github.com>
2022-02-16 22:51:15 +0900

    Create Student.java

---
+++ FAST_JAVA/Chapter02/ch01/Student.java
@@ -0,0 +1,16 @@
+package ch01;
+
+// public 접근제어자 : 공용으로 사용
+public class Student {
+	// 패키지 소문자로 시작
+	// 클래스 대문자로 시작
+	// 메서드 소문자로 시작
+	
+	// default 접근제어자 : 패키지 안에서만 사용 가능
+	// 학생 클래스  속성 생성(iv)
+	int studentNumber;
+	String studentName;
+	int majorCode;
+	String majorName;
+	int grade;
+}

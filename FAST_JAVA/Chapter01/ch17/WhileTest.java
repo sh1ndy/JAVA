ec96a86cd8b184d4f83a36ee5873f90457d04b22
yeol2 <43854344+sh1ndy@users.noreply.github.com>
2022-02-16 22:34:29 +0900

    Create WhileTest.java

---
+++ FAST_JAVA/Chapter01/ch17/WhileTest.java
@@ -0,0 +1,17 @@
+package ch17;
+
+public class WhileTest {
+
+	public static void main(String[] args) {
+		int i = 1;
+		int sum = 0;
+		
+		while (i <= 10) {
+			sum += i;
+			i++;
+		}
+		System.out.println(sum); // 55
+		
+	}
+
+}

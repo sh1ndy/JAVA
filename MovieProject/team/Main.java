a5d3dad6e2e53bd850464bab13dccf45089e2f97
yeol2 <43854344+sh1ndy@users.noreply.github.com>
2022-02-16 17:55:08 +0900

    Create Main.java

---
+++ MovieProject/team/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main(String[] args) {
+		new MainGUI().setVisible(true);
+		
+//		new movieRepository().crateTable();
+//		
+//		new MovieFileCreate().createFolder();
+	}
+
+}

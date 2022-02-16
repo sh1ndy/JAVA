0ee187c8909922978513d645e10b91efbb6716bc
yeol2 <43854344+sh1ndy@users.noreply.github.com>
2022-02-16 22:26:08 +0900

    Create BinaryTest.java

---
+++ FAST_JAVA/ch04/BinaryTest.java
@@ -0,0 +1,23 @@
+package ch04;
+
+public class BinaryTest {
+
+	public static void main(String[] args) {
+
+		// 10진수
+		int num = 10;
+		// 2진수
+		int bNum = 0B1010;
+		// 8진수
+		int oNum = 012;
+		// 16진수
+		int xNum = 0XA;
+		
+		System.out.println("10의 10진수  : " + num);
+		System.out.println("10의 2진수 : " + bNum);
+		System.out.println("10의 8진수 : " + oNum);
+		System.out.println("10의 16진수 : " + xNum);
+		
+	}
+
+}

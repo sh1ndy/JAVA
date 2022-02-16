b5907c8c2452b3f3fe80b7d24bd7c1bcfa595231
yeol2 <43854344+sh1ndy@users.noreply.github.com>
2022-02-16 22:30:40 +0900

    Create LocalVariableInter.java

---
+++ FAST_JAVA/Chapter01/ch09/LocalVariableInter.java
@@ -0,0 +1,18 @@
+package ch09;
+
+public class LocalVariableInter {
+	
+	public static void main(String[] args) {
+		
+		// JDK10 부터 사용 가능
+//		var i = 10;
+//		var j = 10.0;
+//		var str = "test";
+//		
+//		System.out.println(i);
+//		System.out.println(j);
+//		System.out.println(str);
+		
+	}
+
+}

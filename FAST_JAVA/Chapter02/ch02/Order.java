b7e21a7725da1ab08122aec1459b066ccc7972fb
yeol2 <43854344+sh1ndy@users.noreply.github.com>
2022-02-16 23:54:32 +0900

    Add files via upload

---
+++ FAST_JAVA/Chapter02/ch02/Order.java
@@ -0,0 +1,11 @@
+package ch02;
+
+public class Order {
+	// 林巩 努贰胶 加己 积己(iv)
+	int orderId;
+	String buterId;
+	String sellerId;
+	int productId;
+	String orderDate;
+
+}
---
+++ FAST_JAVA/Chapter02/ch02/UserInfo.java
@@ -0,0 +1,11 @@
+package ch02;
+
+public class UserInfo {
+	// 회원 클래스 속성 생성(iv)
+	String userId;
+	String userPassword;
+	String userName;
+	String address;
+	int phoneNumber;
+
+}

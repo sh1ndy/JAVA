23c2597fedf1168b91de8623143903864abc44fa
yeol2 <43854344+sh1ndy@users.noreply.github.com>
2022-02-17 15:26:18 +0900

    Rename FAST_JAVA/Chapter02/DeliveryTest.java to FAST_JAVA/Chapter02/ch08/DeliveryTest.java

---
+++ FAST_JAVA/Chapter02/ch08/DeliveryTest.java
@@ -0,0 +1,12 @@
+package ch08;
+
+public class DeliveryTest {
+
+	public static void main(String[] args) {
+
+		Delivery d = new Delivery(202011020003L, 01023450001, "¼­¿ï½Ã °­³²±¸ ¿ª»ïµ¿ 111-333", "2020-11-02", "13:02:58", 35000, "0003");
+		d.deliveryInfo();
+		
+	}
+
+}

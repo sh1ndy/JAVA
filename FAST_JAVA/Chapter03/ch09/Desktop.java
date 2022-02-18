d8a2f52e0a34545e3dd54176f917b604e23be023
yeol2 <43854344+sh1ndy@users.noreply.github.com>
2022-02-18 17:55:33 +0900

    Add files via upload

---
+++ FAST_JAVA/Chapter03/ch09/Desktop.java
@@ -0,0 +1,15 @@
+package ch09;
+
+public class Desktop extends Computer {
+
+	@Override
+	public void display() {
+		System.out.println("Desktop display");
+	}
+
+	@Override
+	public void typing() {
+		System.out.println("Desktip typing");
+	}
+	
+}

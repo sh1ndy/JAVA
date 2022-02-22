d0e7260e72c5a47d1f6af977d18cf237c9527063
yeol2 <43854344+sh1ndy@users.noreply.github.com>
2022-02-22 20:32:44 +0900

    Add files via upload

---
+++ FAST_JAVA/Chapter05/ch12/Member.java
@@ -0,0 +1,47 @@
+package ch12;
+
+public class Member {
+
+	int memberId;
+	private String memberName;
+
+	public Member(int memberId, String memberName) {
+		super();
+		this.memberId = memberId;
+		this.memberName = memberName;
+	}
+
+	public int getMemberId() {
+		return memberId;
+	}
+	
+	public String getMemberName() {
+		return memberName;
+	}
+
+	@Override
+	public String toString() {
+		return memberName + "님의 아이디는 " + memberId + "입니다.";
+	}
+
+	@Override
+	public int hashCode() {
+		return memberId;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		
+		if (obj instanceof Member) {
+			Member member = (Member) obj;
+			if(memberId == member.getMemberId()) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		return false;
+	}
+	
+}
